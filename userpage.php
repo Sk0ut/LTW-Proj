dba9d22d3bd67f56a43e6d2c3b357ac1a8773d4b
sceptross <afonso.castro11@gmail.com>
2015-11-25 18:12:08 +0000

    Start userpage

---
+++ userpage.php
@@ -0,0 +1,28 @@
+<?php
+	// Includes
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> sceptross's page </title>
+		<meta charset="UTF-8">
+	</head>
+
+	<body>
+		<div id="ownevents">
+			<h1> My events </h1> 
+			
+		</div>
+
+		<div id="eventsentered">
+			<h1> Events I'm in </h1>
+		</div>
+
+		<div id="currentinvites">
+			<h1> My current invites </h1>
+		</div>
+
+	</body>
+
+</html>
