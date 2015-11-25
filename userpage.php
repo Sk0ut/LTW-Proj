fe184369ae67e7fa141e2a0bb4532a2f8236acdd
sceptross <afonso.castro11@gmail.com>
2015-11-25 23:11:15 +0000

    I have abslutely no idea what I did

--- userpage.php
+++ userpage.php
@@ -1,9 +1,9 @@
 <?php
 	include_once('database/connection.php');
   	include_once('database/news.php');
-  	$result = getNews($_GET['id']);
-  	$resultEntered = getNews(2);
-  	$resultInvites = getNews(3);
+  	$resultOwnEvents = getOwnerEvents($_GET['id']);
+  	$resultEntered = getRegisteredEvents($_GET['id']);
+  	//$resultInvites = getNews(3);
 ?>
 
 <!DOCTYPE html>
@@ -17,10 +17,9 @@
 		<div id="ownevents">
 			<h1> My events </h1> 
 
-			<?php foreach( $result as $row) {
-    		echo '<h2>' . $row['title'] . '</h1>';
- 			echo '<p>' . $row['introduction'] . '</p>';
- 			echo '<p>' . $row['fulltext'] . '</p>';		
+			<?php foreach( $resultOwnEvents as $row) {
+    		echo '<h2>' . $row['name'] . '</h1>';
+ 			echo '<p>' . $row['description'] . '</p>';	
  			} ?>
 			
 		</div>
@@ -28,19 +27,13 @@
 		<div id="eventsentered">
 			<h1> Events I'm in </h1>
 			<?php foreach( $resultEntered as $row) {
-    		echo '<h2>' . $row['title'] . '</h1>';
- 			echo '<p>' . $row['introduction'] . '</p>';
- 			echo '<p>' . $row['fulltext'] . '</p>';		
+    		echo '<h2>' . $row['name'] . '</h1>';
+ 			echo '<p>' . $row['description'] . '</p>';		
  			} ?>
 		</div>
 
 		<div id="currentinvites">
 			<h1> My current invites </h1>
-			<?php foreach( $resultInvites as $row) {
-    		echo '<h2>' . $row['title'] . '</h1>';
- 			echo '<p>' . $row['introduction'] . '</p>';
- 			echo '<p>' . $row['fulltext'] . '</p>';		
- 			} ?>
 		</div>
 
 	</body>
