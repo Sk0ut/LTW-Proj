b0309d21d68ba1c0f616dd1bcc9c254beca430ec
sceptross <afonso.castro11@gmail.com>
2015-11-26 00:51:25 +0000

    Add message for when there are no events

--- userpage.php
+++ userpage.php
@@ -1,9 +1,8 @@
 <?php
 	include_once('database/connection.php');
-  	include_once('database/news.php');
+  	include_once('database/event.php');
   	$resultOwnEvents = getOwnerEvents($_GET['id']);
   	$resultEntered = getRegisteredEvents($_GET['id']);
-  	//$resultInvites = getNews(3);
 ?>
 
 <!DOCTYPE html>
@@ -16,24 +15,36 @@
 	<body>
 		<div id="ownevents">
 			<h1> My events </h1> 
-
-			<?php foreach( $resultOwnEvents as $row) {
-    		echo '<h2>' . $row['name'] . '</h1>';
- 			echo '<p>' . $row['description'] . '</p>';	
- 			} ?>
+			<?php
+			if(count($resultOwnEvents) == 0){
+				echo '<p> You currently have no events created! </p>'; 
+			}
+			else { 
+				foreach( $resultOwnEvents as $row) {
+    			echo '<h2>' . $row['name'] . '</h1>';
+ 				echo '<p>' . $row['description'] . '</p>';	
+ 				} 
+ 			}?>
 			
 		</div>
 
 		<div id="eventsentered">
 			<h1> Events I'm in </h1>
-			<?php foreach( $resultEntered as $row) {
-    		echo '<h2>' . $row['name'] . '</h1>';
- 			echo '<p>' . $row['description'] . '</p>';		
+			<?php
+			if(count($resultEntered) == 0){
+				echo '<p> You are currently not participating on any events! </p>';
+			}
+			else {
+				foreach( $resultEntered as $row) {
+    				echo '<h2>' . $row['name'] . '</h1>';
+ 					echo '<p>' . $row['description'] . '</p>';
+ 				}	
  			} ?>
 		</div>
 
 		<div id="currentinvites">
 			<h1> My current invites </h1>
+			<p> You currently have no invites! </p>
 		</div>
 
 	</body>
