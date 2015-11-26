db3f19895aedf4a8300f2ae10a26563db2dfd2b1
João Silva <joaosilva@revtut.net>
2015-11-26 10:24:52 +0000

    Added function to check if token matches. Added IP address to database

--- inc/user.php
+++ inc/user.php
@@ -25,17 +25,24 @@ class User {
     private $_token;
 
     /**
+     * IP Address of the user on last login
+     */
+    private $_ipAddress;
+
+    /**
      * Constructor of User
      * @param username username of the user
      * @param password password of the user
      * @param email email of the user
      * @param token token of the user
+     * @param ipAddress ip address of the user
      */
-    public function __construct($username, $password, $email, $token) {
+    public function __construct($username, $password, $email, $token, $ipAddress) {
         $this->_username = $username;
         $this->_password = $password;
         $this->_email = $email;
         $this->_token = $token;
+        $this->_ipAddress = $ipAddress;
     }
 
     /**
@@ -71,12 +78,45 @@ class User {
     }
 
     /**
+     * Get the ip address of the user
+     * @return ip address of the user
+     */
+    public function getIpAddress() {
+        return $this->_ipAddress;
+    }
+
+    /**
      * Check if a password matches the user's password
-     * @param password password of the user
+     * @param password password to be checked
      * @return true if matches, false otherwise
      */
     public function passwordMatch($password) {
         return password_verify($password, $this->_password);
+    }
+
+    /**
+     * Check if a token matches the user's token and
+     * if the ip address is the same
+     * @param token token to be checked
+     * @return true if matches, false otherwise
+     */
+    public function tokenMatch($token) {
+        return $this->_token === $token &&
+                $this->_ipAddress === $_SERVER['REMOTE_ADDR'];
+    }
+
+    /**
+     * Convert a user to string
+     */
+    public function __toString() {
+        return json_encode(
+            [
+                $this->_username,
+                $this->_password,
+                $this->_email,
+                $this->_token,
+                $this->_ipAddress
+            ]);
     }
 }
 
