820866b30a18525f6829dde6ed557c445a63e4a2
sceptross <up201304205@fe.up.pt>
2015-12-01 02:00:26 +0000

    CSS feito para a minha pagina. VAI TE FODER JOAO

--- userpage.php
+++ userpage.php
@@ -8,12 +8,12 @@
   	$resultOwnEvents = $user->getOwnerEvents();
   	$resultEntered = $user->getRegisteredEvents();
 ?>
-	<title> sceptross's page </title>
+	<link rel="stylesheet" type="text/css" href="public/css/userpage.css" />
 	<meta charset="UTF-8">
 	</head>
 
 	<body>
-		<div id="ownevents">
+		<div id="ownevents" class="ownevents event">
 			<h1> My events </h1> 
 			<?php
 			if(count($resultOwnEvents) == 0){
@@ -21,18 +21,18 @@
 			}
 			else { 
 				foreach( $resultOwnEvents as $row) {
-    			echo '<h2>' . $row['name'] . '</h1>';
+    			echo '<h2>' . $row['name'] . '</h2>';
  				echo '<p>' . $row['description'] . '</p>';	
  				} 
  			}?>
 
- 			<form action="createevent.php">
- 				<input type="submit" value="Create an event" />
+ 			<form action="createevent.php" >
+ 				<input type="submit" class="btn" value="Create an event" />
  			</form>
 			
 		</div>
 
-		<div id="eventsentered">
+		<div id="eventsentered" class="eventsentered event">
 			<h1> Events I'm in </h1>
 			<?php
 			if(count($resultEntered) == 0){
@@ -44,12 +44,17 @@
  					echo '<p>' . $row['description'] . '</p>';
  				}	
  			} ?>
+ 			<form action="createevent.php" >
+ 				<input type="submit" class="btn" value="Find Events" />
+ 			</form>
 		</div>
 
-		<div id="currentinvites">
+		<div id="currentinvites" class="currentinvites event">
 			<h1> My current invites </h1>
 			<p> You currently have no invites! </p>
 		</div>
+
+		<footer> . </footer>
 
 	<?php
 	require_once("public/footer.php");
