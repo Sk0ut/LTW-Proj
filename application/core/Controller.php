65d2dff4f8d7166039916f084a0b12772ec74f21
Sk0ut <poke_flavio@hotmail.com>
2015-12-01 11:21:09 +0000

    Allow arguments for model

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -2,19 +2,19 @@
 
 class Controller
 {
-	protected function model($model)
+	protected function model($model, $args)
 	{
-		if (file_exists("../application/models/" . $model . ".php")
+		if (file_exists("../application/models/" . $model . ".php"))
 		{
 			require_once "../application/models/" . $model . ".php";
 		}
 		
-		return new $model();
+		return (new ReflectionClass($model))->newInstanceArgs($args);
 	}
 	
 	protected function view($view, $data = [])
 	{
-		if (file_exists("../application/views/" + $view . ".php")
+		if (file_exists("../application/views/" + $view . ".php"))
 		{
 			require_once "../application/views/" + $view . ".php";
 		}
