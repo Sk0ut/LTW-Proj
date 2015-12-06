614b6805e64cab42f3403b3b6f12f46d276dfafa
João Silva <joaosilva@revtut.net>
2015-12-06 16:02:36 +0000

    Updated design of event page

--- application/views/event_form.php
+++ application/views/event_form.php
@@ -1,26 +1,33 @@
 <div class="container h-align">
-    <?php if($event->getPrivate() == 0) { ?>
-            <h1 class="title"><i class="fa fa-unlock"></i>  <?php echo $event->getName(); ?></h2>
-    <?php } else { ?>
-            <h1 class="title"><i class="fa fa-lock"></i>  <?php echo $event->getName(); ?></h2>
-    <?php } ?>
+    <div id="description">
+        <?php if($event->getPrivate() == 0) { ?>
+                <h1 class="title"><i class="fa fa-unlock"></i>  <?php echo $event->getName(); ?></h2>
+        <?php } else { ?>
+                <h1 class="title"><i class="fa fa-lock"></i>  <?php echo $event->getName(); ?></h2>
+        <?php } ?>
 
-    <img id="eventImage" src="<?php echo $imageUrl ?>">
-    <div class="description">
-        <h3 id="event_type">
-            <i class="fa fa-calendar-check-o"></i>
-            <?php echo $event->getType(); ?>
-        </h3>
-        <h3 class="event_date">
-            <i class="fa fa-calendar-check-o"></i>
-            <?php echo $event->getDate(); ?>
-        </h3>
-        <h4 id="event_owner">
-            <i class="fa fa-user"></i>
-            Created by: <?php echo $owner->getUsername(); ?>
-        </h4>
-        <p id="event_description">
-            <?php echo $event->getDescription(); ?>
-        </p>
+        <img id="eventImage" src="<?php echo $imageUrl ?>">
+        <div class="description">
+            <h3 id="event_type">
+                <i class="fa fa-hashtag"></i>
+                <?php echo $event->getType(); ?>
+            </h3>
+            <h3 class="event_date">
+                <i class="fa fa-calendar-check-o"></i>
+                <?php echo $event->getDate(); ?>
+            </h3>
+            <h4 id="event_owner">
+                <i class="fa fa-user"></i>
+                Created by: <?php echo $owner->getUsername(); ?>
+            </h4>
+            <p id="event_description">
+                <i class="fa fa-bars"></i>
+                <?php echo $event->getDescription(); ?>
+            </p>
+        </div>
+    </div>
+
+    <div id="members">
+
     </div>
 </div>
