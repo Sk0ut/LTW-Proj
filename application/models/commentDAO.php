7ad07ce8f40653b10834a605c9c6b20bb9612186
sceptross <up201304205@fe.up.pt>
2015-12-06 15:56:09 +0000

    Date fix

--- application/models/commentDAO.php
+++ application/models/commentDAO.php
@@ -1,19 +1,20 @@
 <?php
 
 class CommentDAO {
-    public static function addComment($userId, $threadId, $comment, $commentDate, $parentId){
+    public static function addComment($userId, $threadId, $comment, $parentId){
         $db = Database::getInstance();
 
+
         if($parentId == NULL){
-            $query = "INSERT INTO Comments(userId, threadId, comment, commentDate) VALUES(?, ?, ?, ?)";
-            $params = [$userId, $threadId, $comment, $commentDate];
-            $types = [PDO::PARAM_INT, PDO::PARAM_INT, PDO::PARAM_STR, PDO::PARAM_STR];
+            $query = "INSERT INTO Comments(userId, threadId, comment, commentDate) VALUES(?, ?, ?, datetime(now))";
+            $params = [$userId, $threadId, $comment];
+            $types = [PDO::PARAM_INT, PDO::PARAM_INT, PDO::PARAM_STR];
         }
 
         else {
-            $query = "INSERT INTO Comments(userId, threadId, comment, commentDate, parentId) VALUES(?, ?, ?, ?, ?)";
+            $query = "INSERT INTO Comments(userId, threadId, comment, commentDate, parentId) VALUES(?, ?, ?, datetime(now), ?)";
             $params = [$userId, $threadId, $comment, $commentDate, $parentId];
-            $types = [PDO::PARAM_INT, PDO::PARAM_INT, PDO::PARAM_STR, PDO::PARAM_STR, PDO::PARAM_INT];
+            $types = [PDO::PARAM_INT, PDO::PARAM_INT, PDO::PARAM_STR, PDO::PARAM_INT];
         }
 
         $result = $db->executeUpdate($query, $params, $types);
