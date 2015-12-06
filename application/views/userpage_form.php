b5ff519d4f5d7b1af33296e8d3f5b377360570c9
Sk0ut <poke_flavio@hotmail.com>
2015-12-06 15:50:29 +0000

    Add placeholder for type

--- application/views/userpage_form.php
+++ application/views/userpage_form.php
@@ -70,6 +70,7 @@
 
                 <div class="input-box">
 					<select name="type" id="type" class="input-text" autocomplete="off" placeholder="Type">
+						<option value="" disabled selected>Type</option>
 						<?php foreach($eventTypes as $eventType) { ?>
 							<option value="<?php echo $eventType['id']; ?>"><?php echo $eventType['type']; ?></option>
 						<?php }?>
